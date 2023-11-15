c463ab2ebd5051540aa06b9515695fdb441cf02e
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2023-11-15 23:39:08 +0900

    MemberServiceTest Assertions.assertThat().isEqualTo();

---
+++ src/test/java/snowball/core/member/MemberServiceTest.java
@@ -0,0 +1,18 @@
+package snowball.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+    MemberService memberService=new MemberServiceImpl();
+    @Test
+    void join(){
+        //given
+        Member member=new Member(1L,"memberA",Grade.VIP);
+        //when
+        memberService.join(member);
+        Member findMember=memberService.findMember(1L);
+        //then member와 findMember가 같나? isEqualTo
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
